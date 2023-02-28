0fe19959682f162a20cd345c6bb220d854222b75
Julien Le Mee <julemee@gmail.com>
2023-02-28 18:24:25 -0500

    styling hover card

--- src/components/HoverCard.vue
+++ src/components/HoverCard.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="text-center p-8">
-    <h1 class="text-[#79a032] text-4xl font-bold pb-4" >{{ title }}</h1>
+  <div class="text-center p-10">
+    <div class="w-20 h-20 mx-auto rounded-full bg-[#79a032]">
+      <img class="w-20 h-20 rounded-full" src="/images/lufa-logo.png" alt="lufa logo">
+    </div>
+    <h1 class="text-[#79a032] text-4xl font-bold py-10" >{{ title }}</h1>
     <p class="py-4">{{ text }}</p>
     <p class="py-4">{{ description }}</p>
   </div>
