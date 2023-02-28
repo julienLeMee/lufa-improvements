8243c0a698523f73cb00bf28c6cdfee5c98d08d4
Julien Le Mee <julemee@gmail.com>
2023-02-28 16:15:12 -0500

    hover each icon display diffrent card

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,18 @@ import Magnify from 'vue-material-design-icons/Magnify.vue';
 import HomeOutline from 'vue-material-design-icons/HomeOutline.vue';
 import TrendingUp from 'vue-material-design-icons/TrendingUp.vue';
 import Television from 'vue-material-design-icons/Television.vue';
-import MovieOutline from 'vue-material-design-icons/MovieOutline.vue';
-import Plus from 'vue-material-design-icons/Plus.vue';
+import { ref } from 'vue';
+
+const showCard = ref(false);
+const hoveredIcon = ref(null);
+
+function onIconHover(iconName) {
+  hoveredIcon.value = iconName;
+}
+
+function onIconLeave() {
+  hoveredIcon.value = null;
+}
 </script>
 
 <template>
@@ -12,29 +22,23 @@ import Plus from 'vue-material-design-icons/Plus.vue';
         <img class='absolute w-full h-full object-cover' src="https://images.unsplash.com/photo-1610348725531-843dff563e2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80" alt="vegetables" />
         <img class="absolute top-0 w-[120px] mt-10 ml-10 z-[100]" src="/images/lufa-logo.png" alt="lufa logo">
         <div class='flex flex-col bg-[#79a032]/80 pt-10 z-40 items-center justify-center h-screen absolute'>
-          <div class='rounded w-12 pt-2 mx-10 my-4 border-transparent border-b-4 hover:border-b-white'>
+          <div class='rounded w-12 pt-2 mx-10 my-4 border-transparent border-b-4 hover:border-b-white' @mouseover="onIconHover('magnify'), showCard = true" @mouseleave="onIconLeave(), showCard = false">
             <Magnify fillColor='#ffffff' :size='40' class="cursor-pointer flex items-center justify-center" />
           </div>
-          <div class='rounded w-12 pt-2 mx-10 my-4 border-transparent border-b-4 hover:border-b-white'>
+          <div class='rounded w-12 pt-2 mx-10 my-4 border-transparent border-b-4 hover:border-b-white' @mouseover="onIconHover('HomeOutline'), showCard = true" @mouseleave="onIconLeave(), showCard = false">
             <HomeOutline fillColor='#ffffff' :size='40' class="cursor-pointer flex items-center justify-center" />
           </div>
-          <div class='rounded w-12 pt-2 mx-10 my-4 border-transparent border-b-4 hover:border-b-white'>
+          <div class='rounded w-12 pt-2 mx-10 my-4 border-transparent border-b-4 hover:border-b-white' @mouseover="onIconHover('TrendingUp'), showCard = true" @mouseleave="onIconLeave(), showCard = false">
             <TrendingUp fillColor='#ffffff' :size='40' class="cursor-pointer flex items-center justify-center" />
           </div>
-          <div class='rounded w-12 pt-2 mx-10 my-4 border-transparent border-b-4 hover:border-b-white'>
+          <div class='rounded w-12 pt-2 mx-10 my-4 border-transparent border-b-4 hover:border-b-white' @mouseover="onIconHover('Television'), showCard = true" @mouseleave="onIconLeave(), showCard = false">
             <Television fillColor='#ffffff' :size='40' class="cursor-pointer flex items-center justify-center" />
-          </div>
-          <div class='rounded w-12 pt-2 mx-10 my-4 border-transparent border-b-4 hover:border-b-white'>
-            <MovieOutline fillColor='#ffffff' :size='40' class="cursor-pointer flex items-center justify-center" />
-          </div>
-          <div class='rounded w-12 pt-2 mx-10 my-4 border-transparent border-b-4 hover:border-b-white'>
-            <Plus fillColor='#ffffff' :size='40' class="cursor-pointer flex items-center justify-center" />
           </div>
         </div>
 
-        <div class="absolute w-full h-screen flex items-center px-4 z-10 group">
-          <div class='opacity-1 w-[400px] h-[500px] mx-auto bg-white rounded-xl text-black'>
-
+        <div class="absolute w-full h-screen flex items-center px-4 z-10">
+          <div class='w-[400px] h-[500px] mx-auto bg-white rounded-xl text-black duration-300' :class="{'opacity-100': showCard, 'opacity-0': !showCard}">
+            <h1>{{ hoveredIcon }}</h1>
           </div>
           <div class='w-[500px] h-[700px] mr-20 bg-white rounded-xl text-black'>
             <div class='mx-auto p-10'>
@@ -79,7 +83,7 @@ import Plus from 'vue-material-design-icons/Plus.vue';
                   <p>Téléphone <span class="text-red-600">*</span></p>
                   <input
                     class='p-2 my-2 border rounded'
-                    type="phone"
+                    type="tel"
                     placeholder='(514) 669-3559'
                   />
                 </div>
