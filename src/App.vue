39554ec479b693193e6f2b60264ee3d73a6e35db
Julien Le Mee <julemee@gmail.com>
2023-02-28 17:09:57 -0500

    hover each icon display diffrent card

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import Magnify from 'vue-material-design-icons/Magnify.vue';
 import HomeOutline from 'vue-material-design-icons/HomeOutline.vue';
 import TrendingUp from 'vue-material-design-icons/TrendingUp.vue';
 import Television from 'vue-material-design-icons/Television.vue';
+import HoverCard from './components/HoverCard.vue';
 import { ref } from 'vue';
 
 const showCard = ref(false);
@@ -15,6 +16,29 @@ function onIconHover(iconName) {
 function onIconLeave() {
   hoveredIcon.value = null;
 }
+
+const iconInfo = {
+  magnify: {
+    title: "Titre 1",
+    text: "Texte 1",
+    description: "Description 1"
+  },
+  HomeOutline: {
+    title: "Titre 2",
+    text: "Texte 2",
+    description: "Description 2"
+  },
+  TrendingUp: {
+    title: "Titre 3",
+    text: "Texte 3",
+    description: "Description 3"
+  },
+  Television: {
+    title: "Titre 4",
+    text: "Texte 4",
+    description: "Description 4"
+  }
+};
 </script>
 
 <template>
@@ -22,23 +46,23 @@ function onIconLeave() {
         <img class='absolute w-full h-full object-cover' src="https://images.unsplash.com/photo-1610348725531-843dff563e2c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80" alt="vegetables" />
         <img class="absolute top-0 w-[120px] mt-10 ml-10 z-[100]" src="/images/lufa-logo.png" alt="lufa logo">
         <div class='flex flex-col bg-[#79a032]/80 pt-10 z-40 items-center justify-center h-screen absolute'>
-          <div class='rounded w-12 pt-2 mx-10 my-4 border-transparent border-b-4 hover:border-b-white' @mouseover="onIconHover('magnify'), showCard = true" @mouseleave="onIconLeave(), showCard = false">
+          <div class='rounded w-12 pt-2 mx-10 my-4 border-transparent border-b-4 hover:border-b-white' @mouseover="onIconHover('magnify'), showCard = true, setCardContent('magnify')" @mouseleave="onIconLeave(), showCard = false">
             <Magnify fillColor='#ffffff' :size='40' class="cursor-pointer flex items-center justify-center" />
           </div>
-          <div class='rounded w-12 pt-2 mx-10 my-4 border-transparent border-b-4 hover:border-b-white' @mouseover="onIconHover('HomeOutline'), showCard = true" @mouseleave="onIconLeave(), showCard = false">
+          <div class='rounded w-12 pt-2 mx-10 my-4 border-transparent border-b-4 hover:border-b-white' @mouseover="onIconHover('HomeOutline'), showCard = true, setCardContent('HomeOutline')" @mouseleave="onIconLeave(), showCard = false">
             <HomeOutline fillColor='#ffffff' :size='40' class="cursor-pointer flex items-center justify-center" />
           </div>
-          <div class='rounded w-12 pt-2 mx-10 my-4 border-transparent border-b-4 hover:border-b-white' @mouseover="onIconHover('TrendingUp'), showCard = true" @mouseleave="onIconLeave(), showCard = false">
+          <div class='rounded w-12 pt-2 mx-10 my-4 border-transparent border-b-4 hover:border-b-white' @mouseover="onIconHover('TrendingUp'), showCard = true, setCardContent('TrendingUp')" @mouseleave="onIconLeave(), showCard = false">
             <TrendingUp fillColor='#ffffff' :size='40' class="cursor-pointer flex items-center justify-center" />
           </div>
-          <div class='rounded w-12 pt-2 mx-10 my-4 border-transparent border-b-4 hover:border-b-white' @mouseover="onIconHover('Television'), showCard = true" @mouseleave="onIconLeave(), showCard = false">
+          <div class='rounded w-12 pt-2 mx-10 my-4 border-transparent border-b-4 hover:border-b-white' @mouseover="onIconHover('Television'), showCard = true, setCardContent('Television')" @mouseleave="onIconLeave(), showCard = false">
             <Television fillColor='#ffffff' :size='40' class="cursor-pointer flex items-center justify-center" />
           </div>
         </div>
 
         <div class="absolute w-full h-screen flex items-center px-4 z-10">
           <div class='w-[400px] h-[500px] mx-auto bg-white rounded-xl text-black duration-300' :class="{'opacity-100': showCard, 'opacity-0': !showCard}">
-            <h1>{{ hoveredIcon }}</h1>
+            <HoverCard v-if="hoveredIcon !== null" :title="iconInfo[hoveredIcon]?.title ?? ''" :text="iconInfo[hoveredIcon]?.text ?? ''" :description="iconInfo[hoveredIcon]?.description ?? ''" />
           </div>
           <div class='w-[500px] h-[700px] mr-20 bg-white rounded-xl text-black'>
             <div class='mx-auto p-10'>
---
+++ src/components/HoverCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>{{ text }}</p>
+    <p>{{ description }}</p>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  }
+});
+</script>
